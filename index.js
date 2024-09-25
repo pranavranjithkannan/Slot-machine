24c8185df9437abf39bf1e8444bb9ba018011bf2
Pranav <pranavsandle@gmail.com>
2024-09-25 14:07:54 +0530

    updated comment lines

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// 1.Deposit some money
-// 2.Determine number of line to bet on
-// 3.Collect a bet amount
-// 4.Spin the slot machine
-// 5.check if the user won
-// 6.give the user their winnings
-// 7.play again
+// 1.Deposit some money.
+// 2.Determine number of line to bet on.
+// 3.Collect a bet amount.
+// 4.Spin the slot machine.
+// 5.check if the user won.
+// 6.give the user their winnings.
+// 7.play again.
 
 // function deposit(){
 
